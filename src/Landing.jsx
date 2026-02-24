2039eeb8fe4b7c53fae79ad75dbefcb72ea2bcbf
Venkat Kiran <162780886+VenkatKiran326@users.noreply.github.com>
2026-02-24 23:42:48 +0530

    Update Landing.jsx

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Footer from "./pages/Footer";
-import HowitWorks from "./pages/howitWorks";
+import HowitWorks from "./pages/HowitWorks";
 import Feature from "./pages/Feature";
 import WhyUs from "./pages/WhyUs";
 import Brands from "./pages/Brands";
