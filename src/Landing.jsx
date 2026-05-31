b4e93eb636d3af9be292ba9ecc17b51e40792a58
codebl00dVVK <venkatkiran326@gmail.com>
2026-05-31 10:43:45 +0530

    12

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -4,7 +4,7 @@ import HowitWorks from "./pages/howitWorks";
 import Feature from "./pages/Feature";
 import WhyUs from "./pages/WhyUs";
 import  Hero from "./pages/Hero";
-import HowitWorks from "./pages/HowitWorks";
+// import HowitWorks from "./pages/HowitWorks";
 import './styles/landing.css'
 
 
